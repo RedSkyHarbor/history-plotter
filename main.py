295aa96e749769df280c5ce6695441e493f76b6e
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-01-28 18:44:04 -0500

    Fixed spelling mistake

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import argparse
 import os
 
+def main(args):
+    print(args)
+
 def mkdir(path):
     try:
         os.makedirs(path)
-    except:
+    else:
         pass
 
 if __name__ == "__main__":
-    parser = argparse.ArguementParser()
+    parser = argparse.ArgumentParser()
     home_dir = os.environ.get("HOME","~")
     parser.add_argument("--plot-file", type=str, default="history_plots", help="provide path to where you want plot files saved")
     parser.add_argument("--hist-file", type=str, default="%s/.zsh_history" % home_dir, help="provide path to your history dotfile") 
     args = parser.parse_args()
+    main(args)
 
-def main():
-    pass
 
 
