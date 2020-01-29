9dc0a7025e0a7999c201d22836083c5097a334cc
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-01-29 17:18:24 -0500

    Same as last commit but actually working

--- main.py
+++ main.py
@@ -3,20 +3,26 @@ import shutil
 import os
 
 def main(args):
-    shutil.copyfile(args.history_file, os.path.join(args.hist_file, 'history'))
+    shutil.copyfile(args.hist_file, os.path.join(args.hist_dir, 'history'))
 
-def mkdir(path):
-    try:
-        os.makedirs(path)
-    else:
-        pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     home_dir = os.environ.get("HOME","~")
-    parser.add_argument("--plot-file", type=str, default="history_plots", help="provide path to where you want plot files saved")
-    parser.add_argument("--hist-file", type=str, default="%s/.zsh_history" % home_dir, help="provide path to your history dotfile") 
+    parser.add_argument("--hist-dir", type=str, default="history", help="provide path to where you want your history dotfile copied to")
+    parser.add_argument("--plot-dir", type=str, default="plots", help="provide path to where you want plot files saved")
+    parser.add_argument("--hist-file", type=str, default=f"{home_dir}/.zsh_history", help="provide path to your history dotfile") 
     args = parser.parse_args()
+
+    def mkdir(path):
+        try:
+            os.makedirs(path)
+        except:
+            pass
+
+    mkdir(args.hist_dir)
+    mkdir(args.plot_dir)
+
     main(args)
 
 
