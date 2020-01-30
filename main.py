396aaba45eb187f0b60177c1e4f4646e88d33ebe
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-01-30 18:02:18 -0500

    reads history file into list

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ import os
 
 def main(args):
     shutil.copyfile(args.hist_file, os.path.join(args.hist_dir, 'history'))
+    # TODO Open and parse data from history file
+    with open("./history/history", "r") as f:
+        history = f.readlines()
 
+    for line in history:
+        print(line)
+        break
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     home_dir = os.environ.get("HOME","~")
     parser.add_argument("--hist-dir", type=str, default="history", help="provide path to where you want your history dotfile copied to")
     parser.add_argument("--plot-dir", type=str, default="plots", help="provide path to where you want plot files saved")
-    parser.add_argument("--hist-file", type=str, default=f"{home_dir}/.zsh_history", help="provide path to your history dotfile") 
+    parser.add_argument("--hist-file", type=str, default=f"{home_dir}/.zsh_history", help="provide path to your history dotfile")
     args = parser.parse_args()
 
     def mkdir(path):
